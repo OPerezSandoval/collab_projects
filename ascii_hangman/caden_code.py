d7ec065f7fc0c066c80932048e3a858905cc9914
Caden Lister <cadenlister@gmail.com>
2025-05-10 15:03:39 -0500

    My first commit ever!

--- ascii_hangman/caden_code.py
+++ ascii_hangman/caden_code.py
@@ -3,5 +3,38 @@ import random
 
 word_library = ["charm","creeps", "dreamer", "slippers"]
 
-user_input = input()
+random_word = random.choice(word_library)
+word_length = 0
+number_of_guesses = 0
+failed_guesses = 0
+char_list = []
+
+#user_input = input("Enter a single letter to guess:") # stops code t accept user terminal input
+
+for i in random_word: #determines word length
+    word_length += 1
+    char_list.append(i)
+
+if word_length <= 4:
+    number_of_guesses = 4
+
+elif word_length <= 6:
+    number_of_guesses = 5
+
+elif word_length >= 8:
+    number_of_guesses = 6
+
+
+print("-------------------------------")
+print("-------------------------------")
+print("-- Welcome To Brutal Hangman --")
+print("-------------------------------")
+print("--        GOOD LUCK!         --")
+print("-------------------------------")
+
+
+
+
+
+
 

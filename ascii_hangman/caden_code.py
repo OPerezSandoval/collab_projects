93f8d5846812bf7b7d513d6e650d800dda2a714d
Caden Lister <cadenlister@gmail.com>
2025-05-10 15:51:57 -0500

    The biggest failure

--- ascii_hangman/caden_code.py
+++ ascii_hangman/caden_code.py
@@ -8,6 +8,8 @@ word_length = 0
 number_of_guesses = 0
 failed_guesses = 0
 char_list = []
+good_guesses = []
+bad_guesses = []
 
 #user_input = input("Enter a single letter to guess:") # stops code t accept user terminal input
 
@@ -38,11 +40,28 @@ for i in char_list:
 print("\n")
 
 while number_of_guesses > failed_guesses:
-    user_input = input("Enter a single letter to guess:") # stops code t accept user terminal input
-    if user_input in char_list:
-        print("You Guessed The Letter:", user_input)
-    else:
+
+    user_input = input("Enter a single letter to guess:") # stops code and accept user terminal input
+
+    if user_input in char_list and user_input not in good_guesses: # checks user input versus the characters in the word and in guesses
+        good_guesses.append(user_input)
+        print("You correctly guessed:", user_input)
+        print(char_list.index(user_input))
+    elif user_input in char_list and user_input in good_guesses:
+        print("You have already guessed this letter.")
+    elif user_input not in char_list and user_input not in bad_guesses:
+        bad_guesses.append(user_input)
         failed_guesses += 1
+        print("Bad guess, try again.")
+    elif user_input not in char_list and user_input in bad_guesses:
+        print("This was already a failed guess.")
+
+    if failed_guesses == number_of_guesses:
+        print("YOU ARE A FAILURE!")
+        break
+
+    print(failed_guesses)
+    print(number_of_guesses)
 
 
 

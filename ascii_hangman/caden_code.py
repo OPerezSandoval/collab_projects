cbaab7f6c86b1577a749ca724e0183627574ab2f
Caden Lister <cadenlister@gmail.com>
2025-05-10 16:40:53 -0500

    Cleaned print statements

--- ascii_hangman/caden_code.py
+++ ascii_hangman/caden_code.py
@@ -8,7 +8,7 @@ word_length = 0
 number_of_guesses = 0
 failed_guesses = 0
 char_list = [] #keeps track of individual characters in the word
-good_guesses = [] #keeps track of all worthy characters
+good_guesses = [] #keeps track of all worthy characters in order
 bad_guesses = [] #keeps track of all unworthy characters
 
 #user_input = input("Enter a single letter to guess:") # stops code t accept user terminal input
@@ -36,36 +36,45 @@ print("--        GOOD LUCK!         --")
 print("-------------------------------")
 print("\n")
 
-for i in char_list:
-    print("_", end="")
-print("\n")
 
 while number_of_guesses > failed_guesses:
+
+    for good in good_guesses:
+        print(good, end=" ")
+    print("\n")
 
     user_input = input("Enter a single letter to guess:") # stops code and accept user terminal input
 
     if user_input in char_list and user_input not in good_guesses: # checks user input versus the characters in the word and in guesses
-        index_value = char_list.index(user_input)
-        good_guesses[index_value] = user_input
-        #good_guesses.append(user_input)
+        for char in char_list:
+            print(char)
+            if char == user_input:
+                index_value = char_list.index(char)
+                good_guesses[index_value] = user_input
+                print(index_value)
         print("You correctly guessed:", user_input)
-       # print(char_list.index(user_input))
+
     elif user_input in char_list and user_input in good_guesses:
         print("You have already guessed this letter.")
+
     elif user_input not in char_list and user_input not in bad_guesses:
         bad_guesses.append(user_input)
         failed_guesses += 1
         print("Bad guess, try again.")
+
     elif user_input not in char_list and user_input in bad_guesses:
         print("This was already a failed guess.")
 
     if failed_guesses == number_of_guesses:
         print("YOU ARE A FAILURE!")
         break
+    elif good_guesses == char_list:
+        print("NO HANGING TODAY!")
+        print("Thanks for playing!")
+        break
 
     print(failed_guesses)
     print(number_of_guesses)
-    print (good_guesses)
 
 
 

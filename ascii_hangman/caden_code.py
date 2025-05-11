848c76fffa9ff41e88940335035d05fc548695c7
set <cadenlister@gmail.com>
2025-05-10 22:29:17 -0500

    Changed variable guesses to static 6 like the traditional game and added additional blank space for visual representation.

--- ascii_hangman/caden_code.py
+++ ascii_hangman/caden_code.py
@@ -5,7 +5,6 @@ word_library = ["charm","creeps", "dreamer", "slippers"]
 
 random_word = random.choice(word_library)
 word_length = 0
-number_of_guesses = 0
 failed_guesses = 0
 char_list = [] #keeps track of individual characters in the word
 good_guesses = [] #keeps track of all worthy characters in order
@@ -18,14 +17,6 @@ for i in random_word: #determines word length
     char_list.append(i)
     good_guesses.append("_")
 
-if word_length <= 4:
-    number_of_guesses = 4
-
-elif word_length <= 6:
-    number_of_guesses = 5
-
-elif word_length >= 8:
-    number_of_guesses = 6
 
 
 print("-------------------------------")
@@ -34,11 +25,10 @@ print("-- Welcome To Brutal Hangman --")
 print("-------------------------------")
 print("--        GOOD LUCK!         --")
 print("-------------------------------")
-print("\n")
 
 
 def hangman_status(attempts):
-    if attempts == 0.0:
+    if attempts == 0:
         print(" _____")
         print("|     |")
         print("|")
@@ -46,7 +36,7 @@ def hangman_status(attempts):
         print("|")
         print("|")
         print("______")
-    elif attempts <= 0.15:
+    elif attempts == 1:
         print(" _____")
         print("|     |")
         print("|     O")
@@ -54,45 +44,52 @@ def hangman_status(attempts):
         print("|")
         print("|")
         print("______")
-    elif attempts <= 0.30:
+    elif attempts == 2:
         print(" _____")
         print("|     |")
         print("|     O")
         print("|     |")
-        print("|")
+        print("|     |")
         print("|")
         print(" ______")
-    elif attempts <= 0.60:
+    elif attempts == 3:
         print(" _____")
         print("|     |")
         print("|     O")
-        print("|    \|/")
+        print("|     |/ ")
+        print("|     | ")
+        print("|")
+        print("______")
+    elif attempts == 4:
+        print(" _____")
+        print("|     |")
+        print("|     O")
+        print("|    \|/ ")
         print("|     |")
         print("|")
         print("______")
-    elif attempts <= 0.75:
+    elif attempts == 5:
         print(" _____")
         print("|     |")
         print("|     O")
-        print("|    \|/")
+        print("|    \|/ ")
         print("|     |")
         print("|    / ")
         print("______")
-    elif attempts > 0.80:
+    elif attempts == 6:
         print(" _____")
         print("|     |")
         print("|     O")
-        print("|    \|/")
+        print("|    \|/ ")
         print("|     | ")
         print("|    / \ ")
         print("______")
     return
 
 
-while number_of_guesses > failed_guesses:
+while 7 > failed_guesses:
 
-    percentage_guess = failed_guesses / number_of_guesses
-    hangman_status(percentage_guess)
+    hangman_status(failed_guesses)
 
 
     for good in good_guesses:
@@ -100,7 +97,7 @@ while number_of_guesses > failed_guesses:
     print("\n")
 
     user_input = input("Enter a single letter to guess:") # stops code and accept user terminal input
-
+    print("\n")
     if user_input in char_list and user_input not in good_guesses: # checks user input versus the characters in the word and in guesses
         for index_value, char in enumerate(char_list):
             if char == user_input:
@@ -114,18 +111,24 @@ while number_of_guesses > failed_guesses:
     elif user_input not in char_list and user_input not in bad_guesses:
         bad_guesses.append(user_input)
         failed_guesses += 1
-        if failed_guesses != number_of_guesses:
+        if failed_guesses < 6:
             print("Bad guess, try again.")
 
     elif user_input not in char_list and user_input in bad_guesses:
         print("This was already a failed guess.")
 
-    if failed_guesses == number_of_guesses:
+    if failed_guesses == 7:
         print("YOU ARE A FAILURE!")
         print("THE MAN HANGED!")
         break
     elif good_guesses == char_list:
-        print("NO HANGING TODAY!")
+        print("")
+        hangman_status(failed_guesses)
+        print("")
+        for good in good_guesses:
+            print(good, end=" ")
+        print("\n")
+        print("NO HANGING TODAY!\n")
         print("Thanks for playing!")
         break
 

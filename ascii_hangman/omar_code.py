c33f010a263b3a686819de3e961ebef984d12f96
OPEREZSANDOVAL <PEREZ.OMAR99@YAHOO.COM>
2025-05-11 12:32:55 -0500

    Complete ascii character and menu feature

--- ascii_hangman/omar_code.py
+++ ascii_hangman/omar_code.py
@@ -1,9 +1,39 @@
 import random
-from os import write
 
-# Open the text file and read the output
+# Open the texts files and read the output
 hangman_text_file = open("hangman_text.txt", "r")
+hangman_figures_file = open("hangman_figures.txt", "r")
 hangman_text = hangman_text_file.read()
+hangman_figures = hangman_figures_file.readlines()
+hangman_figures_file.close()
+hangman_text_file.close()
+
+
+# Function to output the hangman character with match case logic
+def hangman_character(current_result):
+    # For loop will iterate through the specified lines in the txt file.
+    match current_result:
+        case 0:
+            for i in hangman_figures[0:10 - 1]:
+                print(i)
+        case 1:
+            for i in hangman_figures[10 - 1:19 - 1]:
+                print(i)
+        case 2:
+            for i in hangman_figures[19 - 1:28 - 1]:
+                print(i)
+        case 3:
+            for i in hangman_figures[28 - 1:37 - 1]:
+                print(i)
+        case 4:
+            for i in hangman_figures[37 - 1:46 - 1]:
+                print(i)
+        case 5:
+            for i in hangman_figures[46 - 1:55 - 1]:
+                print(i)
+        case 6:
+            for i in hangman_figures[55 - 1:64 - 1]:
+                print(i)
 
 
 # Integrate a menu option for the user
@@ -16,7 +46,7 @@ def menu():
     print("3 - LEADERBOARD ")
     print("4 - EXIT GRACEFULLY ")
 
-    # Catch the value exception if selection is not an integer
+    # Catch the ValueError if selection is not an integer
     try:
         selection = int(input("Please make a selection: "))
         selections = [1, 2, 3, 4]
@@ -35,7 +65,7 @@ def menu():
                 case 3:
                     print("Feature coming soon")
                 case 4:
-                    print("You have gracefully left the game my, good sir!")
+                    print("You have gracefully left the game, my good sir!")
                     exit()
     except ValueError:
         print("Please enter a correct value 1,2,3,4!\n")
@@ -43,6 +73,8 @@ def menu():
 
 
 def hangman_game():
+    # Define the user attempts
+    user_attempts = 6
     # Random word selected from list & create a list of the word
     word = str(random.choice(["pie", "documents", "computer", "mouse", "hi"]))
     correct_word = list(word)
@@ -50,13 +82,6 @@ def hangman_game():
     # Keep track of all guessed letters & correct guessed letters
     letters_guessed = str([])
     correct_guessed_letters = []
-
-    # Define the number of attempts based on the word length
-    # ****Need to condense this, maybe one line****
-    if len(word) < 5:
-        user_attempts = 3
-    else:
-        user_attempts = 8
 
     # Main code that will run if user has attempts left
     while user_attempts > 0:
@@ -88,16 +113,20 @@ def hangman_game():
             if first_letter_user_guess not in correct_guessed_letters:
                 correct_guessed_letters += first_letter_user_guess
                 print(f"You have {user_attempts} attempts remaining.")
+                hangman_character(user_attempts)
         else:
             user_attempts -= 1
             print(f"You have {user_attempts} attempts remaining.")
+            hangman_character(user_attempts)
 
         # Compare lengths for the correct word and correct guessed letters
         if len(correct_guessed_letters) == len(correct_word):
             exit(print("You win!"))
 
+    hangman_character(user_attempts)
     exit(print("YOU LOST!!!!"))
 
 
+# Define the entry point of the script
 if __name__ == '__main__':
     menu()
